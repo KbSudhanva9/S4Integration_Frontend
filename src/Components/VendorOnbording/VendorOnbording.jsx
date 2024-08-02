bd7bf6c4b782d23f3f078a2ca793a6f4716e7bad
KbSudhanva9 <bhargava_konakalla@tecnics.com>
2024-08-02 20:50:49 +0530

    completed vendor onbording (vendor details) page modifyed css

--- src/Components/VendorOnbording/VendorOnbording.jsx
+++ src/Components/VendorOnbording/VendorOnbording.jsx
@@ -1,10 +1,39 @@
 import Button from '@mui/material/Button';
 import './Vendor.css'
 import { useNavigate } from 'react-router-dom';
+import api from '../../Utils/ApiCalls/Api';
+import { useState } from 'react';
 
 const VendorOnbording = () => {
 
     const nav = useNavigate();
+
+    const [vdata, setVData] = useState([]);
+
+    const handleStatusSearch = async () => {
+        
+        const statusSearchURL = `${import.meta.env.VITE_CROSS_ORIGIN_URL}${import.meta.env.VITE_VENDOR_ONBORDING_BASE_URL}` + `VendorSet('YASWAN141')`;
+        // `VendorSet('${refNo}')`;
+
+        try {
+            const response = await api.get(statusSearchURL, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Accept': 'application/json',
+                    'Authorization': 'Basic ' + btoa(`${import.meta.env.VITE_SAP_USER_NAME}:${import.meta.env.VITE_SAP_PASSWORD}`),
+                    'X-Requested-With': 'XMLHttpRequest'
+                }
+            });
+            console.log(response);
+            console.log(response.data);
+            console.log(response.data.d);
+            setVData(response.data.d);
+
+        } catch (error) {
+            console.log('Search failed', error);
+        }
+        console.log(vdata);
+    };
 
     return (
         <div>
@@ -21,34 +50,107 @@ const VendorOnbording = () => {
             </header>
             <div className='maincomponent'>
                 <b>User Details</b>
-                <div className='maincomponent'>
-                    <div>
-                        <div className='df'>
-                            <div className='box-margin'>
-                                <p>Title</p>
-                                <p className='display-input' >Mr.</p>
-                            </div>
-                            <div className='box-margin'>
-                                <p>Name1</p>
-                                <p className='display-input' >sudhanva</p>
-                            </div>
-                            <div className='box-margin'>
-                                <p>Name2</p>
-                                <p className='display-input' >bhargava</p>
-                            </div>
-                            <div className='box-margin'>
-                                <p>Name3</p>
-                                <p className='display-input' >konakalla</p>
-                            </div>
-                            <div className='box-margin'>
-                                <p>Name4</p>
-                                <p className='display-input' >kbs</p>
-                            </div>
-                            
+                <div >
+                    <div className='df'>
+                        <div className='box-margin'>
+                            <p>Title</p>
+                            <p className='display-input' >{vdata.Anred}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p>Name1</p>
+                            <p className='display-input' >{vdata.Name1}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p>Name2</p>
+                            <p className='display-input' >{vdata.Name2}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p>Name3</p>
+                            <p className='display-input' >{vdata.Name3}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p>Name4</p>
+                            <p className='display-input' >{vdata.Name4}</p>
+                        </div>
+                    </div>
+                    <div className='df'>
+                        <div className='box-margin'>
+                            <p className='lable'>Contact Person</p>
+                            <p className='display-input' >{vdata.Verkf}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p className='lable'>Email</p>
+                            <p className='display-input' >{vdata.SmtpAddr}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p className='lable'>Street/House No</p>
+                            <p className='display-input' >{vdata.Stras}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p className='lable'>Postal Code/City</p>
+                            <p className='display-input' >{vdata.Pstlz}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p className='lable'>Country</p>
+                            <p className='display-input' >{vdata.Land1}</p>
+                        </div>
+                    </div>
+                    <div className='df'>
+                        <div className='box-margin'>
+                            <p className='lable'>Region</p>
+                            <p className='display-input' >{vdata.Regio}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p className='lable'>Phone No</p>
+                            <p className='display-input' >{vdata.Telf1}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p className='lable'>Fax</p>
+                            <p className='display-input' >{vdata.Telfx}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p className='lable'>Industry Type</p>
+                            <p className='display-input' >{vdata.Brsch}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p className='lable'>Tax Number</p>
+                            <p className='display-input' >{vdata.TaxNumber}</p>
+                        </div>
+                    </div>
+                </div>
+                <b>Bank Details</b>
+                <div >
+                    <div className='df'>
+                        <div className='box-margin'>
+                            <p>Bank Name</p>
+                            <p className='display-input' >{vdata.Bankl}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p>Bank Account No</p>
+                            <p className='display-input' >{vdata.Bankn}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p>IFSC Code</p>
+                            <p className='display-input' >{vdata.Ifsc}</p>
+                        </div>
+                    </div>
+                    <div className='df'>
+                        <div className='box-margin'>
+                            <p className='lable'>Order Currency</p>
+                            <p className='display-input' >{vdata.Waers}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p className='lable'>Minimun Order Value</p>
+                            <p className='display-input' >{vdata.Minbw}</p>
+                        </div>
+                        <div className='box-margin'>
+                            <p className='lable'>Paymet Terms</p>
+                            <p className='display-input' >{vdata.PaymentTerms}</p>
                         </div>
                     </div>
                 </div>
             </div>
+            {/* <Button onClick={handleStatusSearch}>get data</Button> */}
         </div>
     );
 }
